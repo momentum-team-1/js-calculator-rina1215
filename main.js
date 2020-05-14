828ed0cbf0d2287d584b4e2a82c227506ddb7a98
[i2i5] <[i1215@icloud.com]>
2020-05-14 13:51:52 -0400

    final commit

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-const screen = document.getElementById('screen')
+const screen = document.getElementById('display')
 
 function clear_screen () {
   screen.value = ' '
 }
 
 function display_on_screen (x) {
-  screen.value += x
+  screen.value += x //each time I click it will show the number in //store the numbers
 }
 
 function show_result () {
-  let val = screen.value
-  val = eval(val)
-  screen.value = val
-}
+  let res1 = screen.value //stores numbers and operator
+  const res2 = eval(res1) //calculates
+  screen.value = res2     //calculates put reslt2 and display result on the screen
+}  
 
